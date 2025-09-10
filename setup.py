8990a6f029995f50011a8a31add0e3b02d902ff3
Laurent Champagnac <champagnac.laurent@gmail.com>
2025-09-10 09:35:49 +0000

    py 3.13 : remove 3.7 (setuptools in beta, buggy with the toml)

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ p_name = "pysolhttpclient"
 p_author = "Laurent Champagnac"
 p_email = "champagnac.laurent@gmail.com"
 p_url = "https://github.com/champax/pysolhttpclient"
-p_version = "3.13.2"
+p_version = "3.13.3"
 
 setup(
 
