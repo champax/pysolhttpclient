f821fabdb5da47609cf2222925687c3be250c661
Laurent Champagnac <champagnac.laurent@gmail.com>
2025-09-10 09:21:56 +0000

    py 3.13 : allow 3.7

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ p_name = "pysolhttpclient"
 p_author = "Laurent Champagnac"
 p_email = "champagnac.laurent@gmail.com"
 p_url = "https://github.com/champax/pysolhttpclient"
-p_version = "3.13.0"
+p_version = "3.13.2"
 
 setup(
 
